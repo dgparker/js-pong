b5bab0001f79d9d5ed705bd195bf6b9284afac2e
Dylan <dgparker5150@gmail.com>
2017-08-26 14:06:10 -0500

    added ai and comments

--- pong.js
+++ pong.js
@@ -2,13 +2,15 @@ var board;
 var playerScore = 0;
 var aiScore = 0;
 
+//starts the game and generates all pieces
 function start() { 
     board.start();
-    playerPaddle = new paddle(25, 100, 'white', 25, (board.canvas.height/2), 'player');
-    aiPaddle = new paddle(25, 100, 'white', (board.canvas.width - 50), (board.canvas.height/2), 'ai');
+    playerPaddle = new paddle (15, 100, 'white', 25, (board.canvas.height/2), 'player');
+    aiPaddle = new paddle(15, 100, 'white', (board.canvas.width - 50), (board.canvas.height/2), 'ai');
     ball = new ball(15, 15, "white", (board.canvas.width/2), (board.canvas.height/2));
 }
 
+//create are game board
 var board = {
     canvas: document.createElement("canvas"),
     start: function() {
@@ -24,6 +26,7 @@ var board = {
             board.key = false;
         });
     },
+    //clears the board when updating so we don't leave any trails
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         //draw dashed line
@@ -41,6 +44,7 @@ var board = {
     }
 }
 
+//controls for player paddle
 function move(key, paddle){
     if(key && key === 87){
         paddle.speedY -= 5;
@@ -49,6 +53,16 @@ function move(key, paddle){
     }
 }
 
+//controls for ai paddle
+function ai(paddle, ball){
+    if(paddle.y < ball.y){
+        paddle.y += 2;
+    }else if(paddle.y > ball.y){
+        paddle.y -= 2
+    }
+}
+
+//paddle constructor
 function paddle(width, height, color, x, y, name){
     this.name = name;
     this.width = width;
@@ -62,6 +76,7 @@ function paddle(width, height, color, x, y, name){
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    //allows paddles to pass through the canvas appearing at the opposite end
     this.newPos = function() {
         if(this.y < 0 - this.height){
             this.y = board.canvas.height;
@@ -75,24 +90,27 @@ function paddle(width, height, color, x, y, name){
     
 }
 
+//ball constructor function
 function ball(width, height, color, x, y){
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
-    this.speedX = 1;
-    this.speedY = 1;
+    this.speedX = 2;
+    this.speedY = 2;
     this.update = function(){
         ctx = board.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+
+    //resets the ball when somebody scores also responsible for moving the ball
     this.newPos = function() {
         if(this.y < 0){
-            this.speedY = 1;
+            this.speedY = 2;
             this.y += this.speedY;
         }else if(this.y > board.canvas.height - this.height){
-            this.speedY = -1;
+            this.speedY = -2;
             this.y += this.speedY            
         }else if(this.x > board.canvas.width){
             this.x = board.canvas.width/2;
@@ -111,32 +129,30 @@ function ball(width, height, color, x, y){
             this.y += this.speedY;
         }        
     }
+
+    //deflects the ball off of a paddle
     this.deflect = function(paddle) {
-        if(paddle.name === "player" && this.x >= paddle.x && this.x <= paddle.x + paddle.width){
-            if(this.y >= paddle.y && this.y <= paddle.y + paddle.height){
+        if((this.x >= paddle.x && this.x <= paddle.x + paddle.width) || (this.x + this.width >= paddle.x && this.x <= paddle.x + paddle.width)){
+            if((this.y >= paddle.y && this.y <= paddle.y + paddle.height) || (this.y + this.height >= paddle.y && this.y + this.height <= paddle.y + paddle.height)){
                 this.speedX = this.speedX * -1;
                 this.x += this.speedX;
             }            
-        }else if(paddle.name === "ai" && this.x >= paddle.x && this.x <= paddle.x + paddle.width){
-            if(this.y >= paddle.y && this.y <= paddle.y + paddle.height){
-                this.speedX = this.speedX * -1;
-                this.x += this.speedX;
-            }
         }
     }    
 }
 
+//generates a random number for ball speed (-2 or 2) when resetting the ball
 function randomSpeed(){
-    var number = Math.random() < 0.5 ? -1 : 1;
+    var number = Math.random() < 0.5 ? -2 : 2;
     return number;
 }
-
+//updates all pieces of the game
 function updateBoard(){
     board.clear();
     playerPaddle.speedY = 0;
     aiPaddle.speedY = 0;
     move(board.key, playerPaddle);
-    move(board.key, aiPaddle);
+    ai(aiPaddle, ball)
     playerPaddle.newPos();
     playerPaddle.update();
     aiPaddle.newPos();
@@ -147,4 +163,5 @@ function updateBoard(){
     ball.deflect(playerPaddle);
 }
 
+//start the game when fully loaded
 window.onload = start();
